cb2cb269c9f7385c490141fc7fbf6c6465a9cc2c
eyosafit <168662414+eyosafit@users.noreply.github.com>
2024-10-11 14:45:54 -0700

    Update settings.py

--- fans_clone/settings.py
+++ fans_clone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!*of--+n1p4gugm(+d&g)m)5^fpui+ga3um^=364)cdrgfqurj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.eyosafiteliyas.com', '127.0.0.1', 'localhost',]
 
 
 # Application definition
